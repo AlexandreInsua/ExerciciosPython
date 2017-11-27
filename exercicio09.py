9180d73840da982b393d9b892c3f4dff5f1f57bf
Alexandre Insua <alexandreinsua@gmail.com>
2017-11-27 09:48:05 +0100

    Exercicio 9 substitúe unha palabra por outra nun ficheiro .txt

---
+++ exercicio09.py
@@ -0,0 +1,22 @@
+# Pídelle ao usuario dúas palabras. Substitúe a primeira pola segunda nun ficheiro
+#  que se lle pasa como argumento e crea un novo ficheiro cos cambios.
+
+
+words = input("Introduza a palabra que se vai buscar e a substituta: ").split()
+
+print("Palabra a buscar: ", words[0] , ".Palabra substituta: ", words[1])
+file  = open("frases.txt", "r")
+text = file.read()
+print("Texto orixinal:\n")
+print(text)
+# metodo replace() -> devolve unha copia dun String 
+# onde n subcadeas son substituídas por outras n veces
+# (0 para reemprazar en todo o texto)
+text2 = text.replace(words[0], words[1])
+
+file2 = open("frases2.txt", "w")
+file2.write(text2) 
+print("Texto modificado:\n")
+print(text2)
+file.close()
+file2.close()
