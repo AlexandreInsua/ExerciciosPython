86c75018e9e88ec6a12ba88621aae7e87ded2d08
Alexandre Insua <alexandreinsua@gmail.com>
2017-11-28 09:40:58 +0100

    Exercicio 11 - xera números de 1 a 1 millón e gárdaos nun ficheiro

---
+++ exercicio11.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Garda nun ficheiro os números do 1 ao 1 000 000
+
+file = open("numeros.txt", "w")
+for i in range(1,1000001): # O limite superiior é iguan a n+1
+    text = "{} ".format(str(i)) # forta
+    file.write(text)
+file.close()
+
