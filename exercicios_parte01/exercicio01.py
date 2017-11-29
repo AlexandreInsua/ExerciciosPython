9f3d7e25d9e0d4d7b7aa8d38b2d19c3558241816
Alexandre Insua <alexandreinsua@gmail.com>
2017-11-29 08:45:33 +0100

    Exercicios parte 1 - crea carpeta - exercicio01

---
+++ exercicios_parte01/exercicio01.py
@@ -0,0 +1,13 @@
+# Identifica el tipo de dato (int, float, string o list) de los siguientes valores literales
+a = "Hola Mundo" # String
+b = [1, 10, 100] # list de integers
+c = -25          # integer
+d =  1.167       # float
+e = ["Hola", "Mundo"]   # list de Strings
+
+# extra ver tipo de dato
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
