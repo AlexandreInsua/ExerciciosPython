55ff13cb0e2b8188fbb9ae26504920388c5b571e
Alexandre Insua <alexandreinsua@gmail.com>
2017-11-29 12:16:28 +0100

    Exercicios parte2 - exercicio01

---
+++ exercicios_parte02/exercicio01.py
@@ -0,0 +1,19 @@
+# 1) Realiza un programa que lea 2 números por teclado y determine los siguientes aspectos 
+# (es suficiente con mostrar True o False):
+# Si los dos números son iguales
+# Si los dos números son diferentes
+# Si el primero es mayor que el segundo
+# Si el segundo es mayor o igual que el primero
+
+print("Vamos a analizar dous números\n---------------------")
+num1 = int(input("Introduza o primeiro número: "))
+num2 = int(input("Introduza o segundo número: "))
+
+if num1 == num2:
+    print("{} e {} son iguais.".format(num1,num2))
+else:
+    print("Os números son diferentes: ")
+    if num1 < num2:
+        print("{} é maior que {}.".format(num1, num2))
+    else:
+        print("{} é menor que {}.".format(num1, num2))    
