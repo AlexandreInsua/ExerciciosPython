8f45d582a500208bf6a42025a7840e46454c6051
Alexandre Insua <alexandreinsua@gmail.com>
2017-11-29 08:51:52 +0100

    Exercicio 2

---
+++ exercicios_parte01/exercicio02.py
@@ -0,0 +1,14 @@
+# Determina mentalmente (sin programar) el resultado que aparecerá por pantalla a partir de las siguientes variables:
+a = 10
+b = -5
+c = "Hola "
+d = [1, 2, 3]
+
+
+print(a * 5)  # 50
+print(a - b)    # 15
+print(c + "Mundo")  # "Hola Mundo" 
+print(c * 2)        # "Hola Hola "
+print(d[-1])        # 3
+print(d[1:])        # [2, 3]
+print(d + d)        # [1, 2, 3, 1, 2, 3]
