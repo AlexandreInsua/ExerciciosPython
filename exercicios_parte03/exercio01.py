e6c7c124884b0eec81f0b4a015f5057ac262e4ad
Alexandre Insua <alexandreinsua@gmail.com>
2017-11-30 13:21:16 +0100

    Exercicios parte 03 - exercico01

---
+++ exercicios_parte03/exercio01.py
@@ -0,0 +1,11 @@
+#  1) Realiza un programa que lea dos números por teclado y permita elegir entre 3 opciones en un menú:
+# Mostrar una suma de los dos números
+# Mostrar una resta de los dos números (el primero menos el segundo)
+# Mostrar una multiplicación de los dos números
+# En caso de no introducir una opción válida, el programa informará de que no es correcta.
+
+print("Operacións con dous números.")
+num1 = int(input("Inserte un número: "))
+num2 = int(input("Inserte outro número: "))
+opcion = int(input("Inserte a operación que desexe realizar:\n1.- Sumar.\n2.- Restar.\n.3.- Multiplicar"))
+
