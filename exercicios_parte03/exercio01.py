9242d4347488368b93a23a72810a567b8fea8aef
Alexandre Insua <alexandreinsua@gmail.com>
2017-11-30 16:44:41 +0100

    Exercicios parte 3 - exercicio 1

--- exercicios_parte03/exercio01.py
+++ exercicios_parte03/exercio01.py
@@ -4,8 +4,28 @@
 # Mostrar una multiplicación de los dos números
 # En caso de no introducir una opción válida, el programa informará de que no es correcta.
 
+import sys
+
 print("Operacións con dous números.")
 num1 = int(input("Inserte un número: "))
 num2 = int(input("Inserte outro número: "))
-opcion = int(input("Inserte a operación que desexe realizar:\n1.- Sumar.\n2.- Restar.\n.3.- Multiplicar"))
-
+opcion = int(input("Inserte a operación que desexe realizar:\n1.- Sumar.\n2.- Restar.\n.3.- Multiplicar.\n4.- Sair."))
+while opcion != 4:
+    if opcion == 1:
+        resultado = num1 + num2
+        print("A suma de {} e {} é {}".format(num1,num2,resultado))
+        break;
+    elif opcion == 2:
+        resultado = num1 - num2
+        print("A resta de {} menos {} é {}".format(num1,num2,resultado))
+        break;
+    elif opcion == 3:
+        resultado = num1 * num2
+        print("O producto de {} por {} é {}".format(num1,num2,resultado)) 
+        break;
+    elif opcion == 4:
+        print("Fin de programa")
+        sys.exit
+    else:
+        print("A opción debe ser entre 1 e 4.")
+        break;
