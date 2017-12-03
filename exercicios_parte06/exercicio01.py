846fc2dafcf9184944ec2b79a0a8edba74d7183c
Alexandre Insua <alexandreinsua@gmail.com>
2017-12-03 20:12:43 +0100

    Exerpicios parte 6 - exercicio 01

---
+++ exercicios_parte06/exercicio01.py
@@ -0,0 +1,10 @@
+# 1) Realiza una función llamada area_rectangulo() que devuelva el área del rectangulo a partir de una base y una altura.
+#  Calcula el área de un rectángulo de 15 de base y 10 de altura.
+# Nota: El área de un rectángulo se obtiene al multiplicar la base por la altura.
+
+
+def area_rectangulo(base, altura):
+    return base * altura
+
+
+print("A área do rectángulo é {}". format(area_rectangulo(15,10)))
