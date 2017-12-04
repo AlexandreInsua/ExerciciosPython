19d9f3dde079720e1bc7f5e00d4be12c99c18825
Alexandre Insua <alexandreinsua@gmail.com>
2017-12-04 10:11:47 +0100

    parte 07 - exercicio 04

---
+++ exercicios_parte07/exercicio04.py
@@ -0,0 +1,18 @@
+# 4) Localiza el error en el siguiente bloque de código. 
+# Crea una excepción para evitar que el programa se bloquee y además explica en un mensaje al usuario la causa y/o solución:
+# In [10]:
+# Completa el ejercicio aquí
+# resultado = 15 + "20"
+
+# ---------------------------------------------------------------------------
+# TypeError                                 Traceback (most recent call last)
+# <ipython-input-10-4c554866ea5f> in <module>()
+#      1 # Completa el ejercicio aquí
+# ----> 2 resultado = 15 + "20"
+
+# TypeError: unsupported operand type(s) for +: 'int' and 'str'
+
+try:
+    resultado = 15 + "20"
+except TypeError:
+    print("Os operadores son de tipo diferente e non e poder realizar a operación.")
