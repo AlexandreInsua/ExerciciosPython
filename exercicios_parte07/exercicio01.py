3f3617432678425d21b2afd11ec121cea5ff3f08
Alexandre Insua <alexandreinsua@gmail.com>
2017-12-04 10:00:46 +0100

    Parte 7 - exercicio 01

---
+++ exercicios_parte07/exercicio01.py
@@ -0,0 +1,19 @@
+# 1) Localiza el error en el siguiente bloque de código. Crea una excepción para evitar que el programa se bloquee y además explica en un mensaje al usuario la causa y/o solución:
+# In [13]:
+# Completa el ejercicio aquí
+# resultado = 10/0
+
+
+# ---------------------------------------------------------------------------
+# ZeroDivisionError                         Traceback (most recent call last)
+# <ipython-input-13-fa18751f1091> in <module>()
+#       1 # Completa el ejercicio aquí
+# ----> 2 resultado = 10/0
+
+# ZeroDivisionError: division by zero
+
+try:
+    resultado = 10/0
+    print(resultado)
+except ZeroDivisionError:
+    print("O resultado de unha división entre 0 é unha indterminación matemática.")
